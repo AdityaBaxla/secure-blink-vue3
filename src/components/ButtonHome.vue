9110e3d06f7a7b1a3a028a10c8dbdfec84ac1aa7
AdityaBaxla <adityabaxla@gmail.com>
2024-05-07 18:57:11 +0530

    form format fixed

---
+++ src/components/ButtonHome.vue
@@ -0,0 +1,39 @@
+<template>
+  <button id="button-main">{{ text }} <span id="button-icon"></span></button>
+</template>
+
+<script>
+const { defineComponent } = require('vue')
+
+defineComponent({
+  name: 'button-home',
+  data() {
+    return {
+      text: 'Get Started'
+    }
+  }
+})
+</script>
+
+<style scoped>
+#button-main {
+  width: 239px;
+  height: 72px;
+  /* UI Properties */
+  background: #0e77ff 0% 0% no-repeat padding-box;
+  border-radius: 35px;
+  opacity: 1;
+}
+
+#button-icon {
+  top: 5926px;
+  left: 1168px;
+  width: 7px;
+  height: 12px;
+  transform: matrix(0, -1, 1, 0, 0, 0);
+  /* UI Properties */
+  background: transparent url('../assets/img/arrow-down-sign-to-navigate.svg') 0% 0% no-repeat
+    padding-box;
+  opacity: 1;
+}
+</style>
--- src/components/SendUsMessage.vue
+++ src/components/SendUsMessage.vue
@@ -1,11 +1,52 @@
 <template>
-  <div id="s5-form-container" class="grid grid-cols-1 gap-3 justify-items-left px-4 pt-5">
-    <div id="s5-form-top" class="font-pop text-5xl relative">Send Us message</div>
-    <input
-      placeholder="wow"
-      class="relative border-none border-b border-gray-400 focus:border-blue-500 outline-none"
-    />
-    <input placeholder="wow" />
-    <input placeholder="wow" />
+  <div id="s5-form-container" class="grid grid-cols-1 gap-3 justify-items-left px-24 py-20">
+    <div id="s5-form-top" class="font-pop font-semibold text-4xl relative">Send Us message</div>
+    <div class="relative">
+      <input class="placeholder-black w-full py-2 px-3 outline-none" placeholder="Your Email*" />
+      <div class="border-b border-black absolute left-0 bottom-0 w-full"></div>
+    </div>
+    <div class="relative">
+      <input class="placeholder-black w-full py-2 px-3 outline-none" placeholder="Your Name*" />
+      <div class="border-b border-black absolute left-0 bottom-0 w-full"></div>
+    </div>
+    <div class="relative">
+      <input class="placeholder-black w-full py-2 px-3 outline-none" placeholder="Phone Number*" />
+      <div class="border-b border-black absolute left-0 bottom-0 w-full"></div>
+    </div>
+    <div class="relative">
+      <input class="placeholder-black w-full py-2 px-3 outline-none" placeholder="Subject" />
+      <div class="border-b border-black absolute left-0 bottom-0 w-full"></div>
+    </div>
+    <div class="relative">
+      <input
+        class="placeholder-black w-full py-2 px-3 outline-none mb-12"
+        placeholder="Your Message"
+      />
+      <div class="border-b border-black absolute left-0 bottom-0 w-full"></div>
+    </div>
+    <div class="flex items-center">
+      <input
+        type="checkbox"
+        id="terms"
+        name="terms"
+        class="rounded-full h-4 w-4 border-gray-300 focus:ring-blue-500 text-blue-500"
+      />
+      <label for="terms" class="ml-2">Agree to terms</label>
+    </div>
   </div>
 </template>
+
+<style scoped>
+div {
+  position: relative;
+}
+
+input {
+  position: relative;
+}
+
+#s5-form-container {
+  width: 747px;
+  height: 836px;
+}
+</style>
