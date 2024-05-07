fc735d87a86a3a4cbf471a8f4c000a722f533308
AdityaBaxla <adityabaxla@gmail.com>
2024-05-07 17:56:05 +0530

    navbar top made

--- src/components/NavbarHomeTop.vue
+++ src/components/NavbarHomeTop.vue
@@ -4,18 +4,18 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <nav class="navbar flex flex-row w-full justify-between items-center pr-20">
+  <nav class="navbar flex flex-row w-full justify-between items-center ml-60">
     <div>
-      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/" class="text-3xl text-white font-semibold">Home</RouterLink>
     </div>
 
-    <RouterLink to="/login" class="flex flex-row gap-2 text-3xl">
+    <RouterLink to="/login" class="sitems-center nav-right flex flex-row gap-4 text-2xl text-white">
       <span>About</span>
       <span>Services</span>
       <span>Case Studies</span>
       <span>Testimonials</span>
       <span>Contact</span>
-      <span>Login</span>
+      <div class="rounded-md bg-white text-blue-600 p-2 px-4">Login</div>
     </RouterLink>
   </nav>
 </template>
@@ -23,13 +23,17 @@ import { RouterLink, RouterView } from 'vue-router'
 <script lang="ts"></script>
 
 <style scoped>
+.nav-right {
+}
+
 div {
   position: relative;
 }
 .navbar {
   position: relative; /* Position the navbar relative to the viewport */
   z-index: 9999; /* Ensure the navbar is on top of everything */
-  width: 1920px;
+  width: 1358px;
+  top: 75px;
   height: 53px;
   background-color: transparent; /* Example background color */
 }
